6bfbedb3210052eaba3ce66ce1917cdff1460c94
Hemant Kumar Srivastava <hemantmgs7275@gmail.com>
2025-07-01 18:22:01 +0530

    KeyPress & cheackAnswer function added

--- index.js
+++ index.js
@@ -8,47 +8,87 @@ var gamePattern = [];
 // Variable for user clicked pattern
 var userClickedPattern = [];
 
-//Check which button is pressed
-$(".btn").click(function(){
-    var userChoosenColor = $(this).attr("id");
-    userClickedPattern.push(userChoosenColor);
+var started = false;
+var level = 0;
 
-    // Play the sound of the selected color
-    playAudio(userChoosenColor);
-    // Animate the button press
-    animatePress(userChoosenColor);
+$(document).keypress(function () {
+  if (level === 0) {
+    $("#level-title").text("level " + level);
+    nextSequence();
+  }
 });
 
-// animation for the button press
-function animatePress(currentColor){
-    $("#"+currentColor).addClass("pressed");
-    setTimeout(()=>{
-        $("#"+currentColor).removeClass("pressed");
-    },100); //delay of 1-second
+//Check which button is pressed
+$(".btn").click(function () {
+  var userChoosenColor = $(this).attr("id");
+  userClickedPattern.push(userChoosenColor);
+
+  // Play the sound of the selected color
+  playAudio(userChoosenColor);
+  // Animate the button press
+  animatePress(userChoosenColor);
+});
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    } else{
+        playAudio("wrong");
+        $("body").addClass("game-over");
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);   
+        startOver();
+    }
 }
 
+
 //function for generating Random Number....
-function nextSequence(){
-    var randomNumber = Math.floor(Math.random() * 4);
-    
-    // Random Color Selection from ButtonColors Array by nextSequence Function
-    var randomChoosenColor = buttonColors[randomNumber];
-    
-    // Push the selected color to the game pattern
-    gamePattern.push(randomChoosenColor);
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("level " + level);
+  //
+  var randomNumber = Math.floor(Math.random() * 4);
 
-    // Animate the selected color
-    $("#"+randomChoosenColor).fadeOut(100).fadeIn(100).fadeOut(100).FadeIn(100);
+  // Random Color Selection from ButtonColors Array by nextSequence Function
+  var randomChoosenColor = buttonColors[randomNumber];
 
-    // Play the sound of the selected color
-    playAudio(randomChoosenColor);
+  // Push the selected color to the game pattern
+  gamePattern.push(randomChoosenColor);
 
-    // Return the selected color
+  // Animate the selected color
+  $("#" + randomChoosenColor).fadeOut(100).fadeIn(100).fadeOut(100).FadeIn(100);
+
+  // Play the sound of the selected color
+  playAudio(randomChoosenColor);
+
+  // Return the selected color
+}
+
+
+// animation for the button press
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(() => {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100); //delay of 1-second
 }
 
 
 // Function to play audio for the selected color
-function playAudio(name){
-        var audio = new Audio("sounds/"+ name +".mp3");
-        audio.play();
-};
+function playAudio(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
